ad24d42614f7bec4e1de97818ac102f5b57bc145
naochael-jordan <naochael.jordan@gmail.com>
2018-10-30 07:20:12 +0900

    add typescript data

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,116 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        engbook
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+    <header>
+      <h1>Engbook</h1>
+    </header>
+
+    <div class="main">
+      <h2>TypeScript</h2>
+      <ul>
+        <li v-for="(item, index) in list" :key="index">
+          <p class="rank">
+            <span>{{ index + 1 }}</span>
+            位
+          </p>
+          <p class="word">{{ item.word }}</p>
+          <p class="num">
+            <span>{{ item.num }}</span>
+            回
+          </p>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from "~/components/AppLogo.vue";
 
 export default {
   components: {
     AppLogo
+  },
+
+  data() {
+    return {
+      list: []
+    };
+  },
+
+  created() {
+    const results = this.$store.state.ts
+      .trim()
+      .toLowerCase()
+      .replace(/ +/g, " ")
+      .replace(/(\r?\n)?(,?\.?)?/g, "")
+      .split(" ")
+      .reduce((acc, crr) => {
+        const index = acc.findIndex(a => a.word === crr);
+
+        if (index === -1) {
+          acc.push({
+            word: crr,
+            num: 1
+          });
+        } else {
+          acc[index].num += 1;
+        }
+
+        return acc;
+      }, [])
+      .sort((a, b) => (a.num > b.num ? -1 : 1));
+    this.list = results;
+    console.log(results);
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+}
+
+header {
+  position: relative;
+  padding: 12px 0;
   text-align: center;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+h2 {
+  padding: 4px 0 4px 8px;
+  font-size: 16px;
+  background-color: #efefef;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+ul {
+  padding: 0;
+  list-style: none;
 }
 
-.links {
-  padding-top: 15px;
+li {
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  border-bottom: 1px solid #efefef;
+}
+
+.rank {
+  margin-right: 10px;
+
+  span {
+    font-size: 20px;
+  }
+}
+
+.word {
+  flex: 1;
+  font-size: 30px;
+}
+
+.num {
+  span {
+    font-size: 20px;
+  }
 }
 </style>
 
